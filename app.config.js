dbfb261aa5830b72108cda0bc6a8506852b046bf
Tashrik Anam <anampartho31@gmail.com>
2023-09-19 11:30:45 +0600

    Remove unused card

--- app.config.js
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-
-export default {
-  name: "native-jobs",
-  version: "0.0.1",
-  extra: {
-    RAPID_API_KEY: process.env.RAPID_API_KEY,
-  },
-};
--- env.js
+++
@@ -1,3 +0,0 @@
-import Constants from "expo-constants";
-
-export const { RAPID_API_KEY } = Constants.manifest2.extra;
